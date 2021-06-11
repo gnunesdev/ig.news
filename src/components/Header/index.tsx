cfff3ff5120ef72819cb52fedddb833924fcf1c4
Guilherme Nunes <gnunesinf@gmail.com>
2021-06-11 16:50:56 -0300

    feat: finalizado fluxo de inscrição no blog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
+import Link from "next/link";
+
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Link href="/">
+          <img src="/images/logo.svg" alt="ig.news" />
+        </Link>
         <nav>
           <a className={styles.linkActive}>Home</a>
           <a>Posts</a>
